bb203482c663ecf2d32db3504c9b4554f91c0b5f
Marcus Nygård <37084190+Makkkkus@users.noreply.github.com>
2019-09-03 15:16:38 +0200

    Removed uneccisary files and changed maven repository from paper to spigot.

--- src/com/statistics/commands/TestCommand.java
+++ src/com/statistics/commands/TestCommand.java
@@ -9,13 +9,29 @@ public class TestCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player p = ((Player) sender);
-			
-			p.sendMessage("Common Statistics is working");
+		if (!(sender instanceof Player)) return false;
+		Player p = ((Player) sender);
+
+		// Checks for arguments
+		for (String arg : args) {
+			switch (arg) {
+			case "arg1":
+				p.sendMessage("arg1");
+				break;
+
+			case "arg2":
+				p.sendMessage("arg2");
+				break;
+
+			default:
+				return false;
+			}
 			return true;
 		}
-		return false;
-	}
 
+		// The code below runs when there are no arguments
+		p.sendMessage("No arguments");
+
+		return true;
+	}
 }
